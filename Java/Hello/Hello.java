5e32ac280decab5a8836846e14b708b5750754db
Zac Zuppas <zzuppas@gmail.com>
2016-06-20 20:59:57 -0600

    Initial commit

---
+++ Java/Hello/Hello.java
@@ -0,0 +1,11 @@
+/* This is a class to print hello.
+*/
+package Hello;
+import java.lang.*;
+
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("hello my java!");
+  }
+}
+
