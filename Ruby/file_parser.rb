4de2e783dcdd88b17b95ab68c5f3363c09a70d26
Zac Zuppas <zzuppas@gmail.com>
2016-06-25 00:11:43 -0600

    Added file churner class

--- Ruby/file_parser.rb
+++ Ruby/file_parser.rb
@@ -1,34 +1,41 @@
 # Copyright RDX
 # Description: Reads all files in path and creates new style.
 
+class FileChurner
+  def initialize
+    #@description = []
+    @line = ''
+  end
+  def feed(line)
+    @line = line
+  end
+  def retrieve
+    @line
+  end
+end
+
 def process_file(filename)
   output = open(filename + '~', 'w')
+  churner = FileChurner.new
   File.open(filename, 'r') do |f|
     f.each_line do |line|
-      output.write(line)
+      churner.feed line
+      output.write(churner.retrieve)
       puts line
     end
   end
   output.close
 end
 
-
-# puts 'file parser: Opens cpp/h files and reformats them'
 #ARGV.each_with_index do |a, i|
 #  puts "Argument #{i}: #{a}"
 #end
-# puts Dir[ARGV[1] + "*"]
-#output = open('output.cpp
+
 puts "Processing path #{ARGV[0]}"
 Dir.foreach(ARGV[0]) do |filename|
   next if filename == '.' or filename == '..'
-  next unless filename.end_with? ".cpp" or filename.end_with? ".h"  
+  next unless filename.end_with? ".cpp" or filename.end_with? ".h"
   next if File.directory? filename
   puts 'file: ' + filename
   process_file(filename)
 end
-
-
-
-
-
